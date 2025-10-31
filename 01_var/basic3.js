52a58e1d8651a753e234e50333af4f81422fedae
Duny <byung243@naver.com>
2025-10-31 14:19:19 +0900

    세번째 커밋

---
+++ 01_var/basic3.js
@@ -0,0 +1,12 @@
+let foods =['감자탕', '짜장면', '탕수육', '햄버거'];
+console.log(foods);
+
+foods[1] = '짬뽕';
+console.log(foods);
+
+foods[4]='삼겹살';
+console.log(foods);
+
+let points = [100,123,211,305];
+let result = points[0] + points[3];
+console.log(result);
