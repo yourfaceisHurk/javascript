1b2f7e6c7054fc73408ecf0f01031aaa5f1ffef5
Duny <byung243@naver.com>
2025-10-31 14:15:12 +0900

    두번째 커밋

--- 01_var/basic.js
+++ 01_var/basic.js
@@ -1,1 +1,27 @@
+//v1.0 -> 2.0
+//
+
 console.log('자바스크립트시작')
+//alert('경고창');
+//confirm('저장하겠습니까?');
+//prompt('이름을 입력하세요','아무개');
+//var name = prompt('이름을입력해주세요');
+document.writeln('<h1>김병준<h1>');
+console.log('콘솔창에 출력');
+
+//변수. 자료형(datatype)
+let myName = '김병준'; //문자열
+let myPoint = 120; // 숫자
+let isAdult = true; // 불린
+let myHobbies = ['독서', '여행', '요리']; //배열
+let myAssets = { //객체
+  car: 'sonata',
+  house: '아파트',
+  cash: 1000000
+}
+
+let test = ['하이',23,true];
+
+let myFamily;
+
+console.log(typeof myName);
---
+++ 01_var/basic2.js
@@ -0,0 +1,26 @@
+//basic2.js
+let num1 = 10; //숫자 리터럴
+let num2 = 20;
+
+console.log('두 변수의 + 연산결과 : ' + (num1 + num2));
+
+
+let str1 = 'Hello, '; // 문자 리터럴
+let str2 = 'world'
+
+console.log('두 변수의 + 연산결과 : ' + str1 + str2);
+
+let temp1 = prompt('값을 입력하세요!!');
+let temp2 = prompt('값을 입력하세요!!');
+
+console.log('입력값은 ' + temp1);
+console.log('입력값은 ' + temp2);
+
+// console.log(temp1 +temp2); //alret으로 받는 데이터는 문자열
+// // console.log(parseInt(temp1) + parseInt(temp2));
+// console.log(temp1 - temp2); // -> 자바스크립트가 알아서 형변환 해줌
+//NaN -> not a number
+
+console.log(temp1 == temp2);
+
+
